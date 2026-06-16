b292ab5c6a78f161f3304fb098b50c385e8c783c
sc990389-sketch <sc990389@gmail.com>
2026-06-16 11:12:39 +0530

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+  card.addEventListener("mouseenter", () => {
+
+    console.log("Git Reset Section");
+
+  });
+
+});
